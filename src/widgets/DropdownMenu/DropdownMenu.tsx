d1815f7c747ca5ec1cbc2c64e02e465caadde6d3
Ivan Besedin <ivanbesedin.05@gmail.com>
2024-04-25 16:03:06 +0300

    edited tabs

--- src/widgets/DropdownMenu/DropdownMenu.tsx
+++ src/widgets/DropdownMenu/DropdownMenu.tsx
@@ -1,19 +1,20 @@
 import styles from './DropdowmMenu.module.scss'
 import footer_icon_big from '../../assets/icons/footer_icon_big.svg'
 import footer_icon_small from '../../assets/icons/footer_icon_small.svg'
+import { NavLink } from 'react-router-dom'
 
 function DropdownMenu() {
   return (
 	<div className={styles.menu}>
-		<p className={styles.text}>Пункт 1</p>
+		<NavLink to={"services"} className={styles.text}>Пункт 1</NavLink>
 		<p className={styles.line}></p>
-		<p className={styles.text}>Новый пункт 2</p>
+		<NavLink to={"services"}className={styles.text}>Новый пункт 2</NavLink>
 		<p className={styles.line}></p>
-		<p className={styles.text}>Еще один пункт 3</p>
+		<NavLink to={"services"} className={styles.text}>Еще один пункт 3</NavLink>
 		<p className={styles.line}></p>
-		<p className={styles.text}>Снова пункт 4</p>
+		<NavLink to={"services"} className={styles.text}>Снова пункт 4</NavLink>
 		<p className={styles.line}></p>
-		<p className={styles.text}>Последний пункт 5</p>
+		<NavLink to={"services"} className={styles.text}>Последний пункт 5</NavLink>
 		<div style={{position: "relative"}}>
 			<div className={styles.icon__wrappper}>
 				<img src={footer_icon_small} alt="" className={styles.icon_left}/>
